d27439fe6cdda2043bbd607e23e56268159aa7a3
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-06-27 19:44:16 -0300

    chore: update all

--- 11_Spring_boot/vendas/src/main/java/io/github/alexsandro/Main.java
+++ 11_Spring_boot/vendas/src/main/java/io/github/alexsandro/Main.java
@@ -1,7 +1,14 @@
 package io.github.alexsandro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class principal da Aplicação.
+ */
+@SpringBootApplication
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    SpringApplication.run(Main.class, args);
   }
 }
