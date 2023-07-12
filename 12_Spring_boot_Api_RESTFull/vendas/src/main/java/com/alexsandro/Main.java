a34244fae7060221a1748b6aba949dd78079e6ed
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-12 20:11:55 -0300

    style: remoção de importações inúteis

--- 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/Main.java
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/Main.java
@@ -1,7 +1,12 @@
 package com.alexsandro;
 
+import com.alexsandro.domain.entity.Cliente;
+import com.alexsandro.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
