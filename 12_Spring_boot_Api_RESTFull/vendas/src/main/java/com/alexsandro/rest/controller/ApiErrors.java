a2d321e40ee2327ab8a5fd77e4e19f8900f9f9b4
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-20 20:55:19 -0300

    feat: Classes para tratar exceptions da aplicação
    trata as exceptins levantadas pela aplicação.

---
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/ApiErrors.java
@@ -0,0 +1,19 @@
+package com.alexsandro.rest.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+
+/**
+ * The type Api errors.
+ */
+public class ApiErrors {
+
+  @Getter
+  private List<String> errors;
+
+  public ApiErrors(String errorMessage) {
+    this.errors = Arrays.asList(errorMessage);
+  }
+
+}
---
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.alexsandro.rest.controller;
+
+import com.alexsandro.domain.exception.RegraNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * The type Application controller advice.
+ */
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+  /*
+  * Sempre que ocorrer uma excepion do tipo RegraNegocioException
+  * a aplicação vai capturar e tratar com esse método.
+  * */
+  @ExceptionHandler(RegraNegocioException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+    String errorMessage = ex.getMessage();
+    return new ApiErrors(errorMessage);
+  }
+}
