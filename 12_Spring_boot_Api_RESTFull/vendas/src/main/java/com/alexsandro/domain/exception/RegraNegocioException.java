93dfdbbc12db060c61893d07795e022f84067e72
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-19 21:30:54 -0300

    feat(exception): adiciona uma exception customizada
    para ser lançada em exceções das regras de negócio.

---
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/domain/exception/RegraNegocioException.java
@@ -0,0 +1,10 @@
+package com.alexsandro.domain.exception;
+
+/**
+ * The type Regra negocio exception.
+ */
+public class RegraNegocioException extends RuntimeException {
+  public RegraNegocioException(String message) {
+    super(message);
+  }
+}
