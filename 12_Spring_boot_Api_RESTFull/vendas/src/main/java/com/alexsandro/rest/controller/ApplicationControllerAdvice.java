71439a60124d833bfd88c372141c3fc4a4d67c78
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-21 20:10:12 -0300

    feat: adiciona método no advice que captura errors
    para pegar as exceptions de NotFoundException

--- 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/ApplicationControllerAdvice.java
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/ApplicationControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.alexsandro.rest.controller;
 
+import com.alexsandro.domain.exception.NotFoundException;
 import com.alexsandro.domain.exception.RegraNegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,13 @@ public class ApplicationControllerAdvice {
   @ExceptionHandler(RegraNegocioException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+    String errorMessage = ex.getMessage();
+    return new ApiErrors(errorMessage);
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ApiErrors handleNotFoundException(NotFoundException ex) {
     String errorMessage = ex.getMessage();
     return new ApiErrors(errorMessage);
   }
