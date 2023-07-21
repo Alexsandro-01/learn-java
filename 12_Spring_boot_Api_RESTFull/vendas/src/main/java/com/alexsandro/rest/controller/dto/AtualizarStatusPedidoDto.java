d8523758b1b79b6e09af4969c19f14c2588578f4
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-21 20:04:28 -0300

    feat: dto para requisições de atualização do status do pedido

---
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/dto/AtualizarStatusPedidoDto.java
@@ -0,0 +1,13 @@
+package com.alexsandro.rest.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * The type Atualizar status pedido dto.
+ */
+@Getter
+@Setter
+public class AtualizarStatusPedidoDto {
+  private String novoStatus;
+}
