4281d679f783fc93ae37c7b5771179c027a16467
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-21 19:03:28 -0300

    feat: adicona assinatura de método para obter pedido completo na interface de PedidoService

--- 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/domain/service/PedidoService.java
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/domain/service/PedidoService.java
@@ -2,8 +2,14 @@ package com.alexsandro.domain.service;
 
 import com.alexsandro.domain.entity.Pedido;
 import com.alexsandro.rest.controller.dto.PedidoDto;
+import java.util.Optional;
 
+/**
+ * The interface Pedido service.
+ */
 public interface PedidoService {
 
   Pedido salvar(PedidoDto dto);
+
+  Optional<Pedido> obterPedidoCompleto(Integer id);
 }
