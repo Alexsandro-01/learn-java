b3178348446cb485070706b482fb0e4b3cc5e30e
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-21 20:06:28 -0300

    feat: criação da assinatura do método atualizarStatus
    na interface de service da rota de pedidos

--- 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/domain/service/PedidoService.java
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/domain/service/PedidoService.java
@@ -1,6 +1,7 @@
 package com.alexsandro.domain.service;
 
 import com.alexsandro.domain.entity.Pedido;
+import com.alexsandro.domain.enums.StatusPedido;
 import com.alexsandro.rest.controller.dto.PedidoDto;
 import java.util.Optional;
 
@@ -12,4 +13,6 @@ public interface PedidoService {
   Pedido salvar(PedidoDto dto);
 
   Optional<Pedido> obterPedidoCompleto(Integer id);
+
+  void atualizaStatus(Integer id, StatusPedido status);
 }
