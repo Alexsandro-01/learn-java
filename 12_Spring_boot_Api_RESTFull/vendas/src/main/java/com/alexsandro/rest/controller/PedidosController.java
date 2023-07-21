7cc78d9b57768f8b8dc76b2db5b5372c05a5b73b
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-07-21 19:04:58 -0300

    feat: Método para obter um pedido completo no controller da rota de pedidos

--- 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/PedidosController.java
+++ 12_Spring_boot_Api_RESTFull/vendas/src/main/java/com/alexsandro/rest/controller/PedidosController.java
@@ -1,10 +1,21 @@
 package com.alexsandro.rest.controller;
 
+import com.alexsandro.domain.entity.ItemPedido;
 import com.alexsandro.domain.entity.Pedido;
 import com.alexsandro.domain.service.PedidoService;
+import com.alexsandro.rest.controller.dto.InformacaoItemPedidoDto;
+import com.alexsandro.rest.controller.dto.InformacoesPedidoDto;
 import com.alexsandro.rest.controller.dto.PedidoDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * The type Pedidos controller.
@@ -31,5 +42,47 @@ public class PedidosController {
     Pedido pedido = service.salvar(dto);
 
     return pedido.getId();
+  }
+
+  /**
+   * Get pedido by id.
+   *
+   * @param id the pedido's id
+   * @return the pedido by id
+   */
+  @GetMapping("/{id}")
+  public InformacoesPedidoDto getById(@PathVariable Integer id) {
+    return service
+        .obterPedidoCompleto(id)
+        .map(pedido -> converter(pedido))
+        .orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pedido não encontrado.")
+        );
+  }
+
+  private InformacoesPedidoDto converter(Pedido pedido) {
+    return InformacoesPedidoDto
+        .builder()
+        .id(pedido.getId())
+        .data(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyy")))
+        .clientName(pedido.getCliente().getNome())
+        .total(pedido.getTotal())
+        .items(converter(pedido.getItens()))
+        .build();
+  }
+
+  private List<InformacaoItemPedidoDto> converter(Set<ItemPedido> itens) {
+    if (CollectionUtils.isEmpty(itens)) {
+      return Collections.emptyList();
+    }
+
+    return itens.stream()
+        .map(itemPedido -> InformacaoItemPedidoDto
+            .builder()
+            .precoUnitario(itemPedido.getProduto().getPreco())
+            .quantidade(itemPedido.getQuantidade())
+            .build()
+        )
+        .collect(Collectors.toList());
   }
 }
